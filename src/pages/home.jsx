1195ccb64919f3349cd42a0bc6f40fbdd1a484e3
Ayushman Kumar Pandey <120387818+ayushman075@users.noreply.github.com>
2024-01-22 17:04:34 +0530

    Add files via upload

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../component/navbar'
+import QueryForm from '../component/queryForm'
+
+const Home = () => {
+  return (
+   <>
+   <Navbar/>
+   <QueryForm/>
+   </>
+  )
+}
+
+export default Home
